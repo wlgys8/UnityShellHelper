7b3d3541698db09d16909dbd66d5d736d6c16522
zilch <wlgys8@gmail.com>
2015-10-23 11:59:38 +0800

    Shell Helper for unity. support win and OS X

---
+++ Editor/ShellHelper.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class ShellHelper  {
+
+	public class ShellRequest{
+		public event System.Action<int,string> onLog;
+		public event System.Action onError;
+		public event System.Action onDone;
+
+		public void Log(int type,string log){
+			if(onLog != null){
+				onLog(type,log);
+			}
+			if(type == 1){
+				UnityEngine.Debug.LogError(log);
+			}
+		}
+
+		public void NotifyDone(){
+			if(onDone != null){
+				onDone();
+			}
+		}
+
+		public void Error(){
+			if(onError != null){
+				onError();
+			}
+		}
+	}
+
+	
+	private static string shellApp{
+		get{
+			#if UNITY_EDITOR_WIN
+			string app = "cmd.exe";
+			#elif UNITY_EDITOR_OSX
+			string app = "bash";
+			#endif
+			return app;
+		}
+	}
+
+
+	private static List<System.Action> _queue = new List<System.Action>();
+
+
+	static ShellHelper(){
+		_queue = new List<System.Action>();
+		EditorApplication.update += OnUpdate;
+	}
+	private static void OnUpdate(){
+		for(int i = 0;i<_queue.Count;i++){
+			try{
+				_queue[i]();
+			}catch(System.Exception e){
+				UnityEngine.Debug.LogException(e);
+			}
+		}
+		_queue.Clear();
+	}
+
+
+
+	public static ShellRequest ProcessCommand(string cmd,string workDirectory,List<string> environmentVars = null){
+		ShellRequest req = new ShellRequest();
+		System.Threading.ThreadPool.QueueUserWorkItem(delegate(object state) {
+			Process p = null;
+			try{
+				ProcessStartInfo start = new ProcessStartInfo(shellApp);
+
+				#if UNITY_EDITOR_OSX
+				string splitChar = ":";
+				start.Arguments = "-c";
+				#elif UNITY_EDITOR_WIN
+				string splitChar = ";";
+				start.Arguments = "/c";
+				#endif
+
+				if(environmentVars != null){
+					foreach(string var in environmentVars){
+						start.EnvironmentVariables["PATH"] += (splitChar + var);
+					}
+				}
+
+				start.Arguments += (" \"" + cmd + " \"");
+				start.CreateNoWindow = true;
+				start.ErrorDialog = true;
+				start.UseShellExecute = false;
+				start.WorkingDirectory = workDirectory;
+
+				if(start.UseShellExecute){ 
+					start.RedirectStandardOutput = false;
+					start.RedirectStandardError = false;
+					start.RedirectStandardInput = false;
+				} else{
+					start.RedirectStandardOutput = true;
+					start.RedirectStandardError = true;
+					start.RedirectStandardInput = true;
+					start.StandardOutputEncoding = System.Text.UTF8Encoding.UTF8;
+					start.StandardErrorEncoding = System.Text.UTF8Encoding.UTF8;
+				}
+
+				p = Process.Start(start);
+				p.ErrorDataReceived += delegate(object sender, DataReceivedEventArgs e) {
+					UnityEngine.Debug.LogError(e.Data);
+				};
+				p.OutputDataReceived += delegate(object sender, DataReceivedEventArgs e) {
+					UnityEngine.Debug.LogError(e.Data);
+				};
+				p.Exited += delegate(object sender, System.EventArgs e) {
+					UnityEngine.Debug.LogError(e.ToString());
+				};
+
+				bool hasError = false;
+				do{
+					string line = p.StandardOutput.ReadLine();
+					if(line == null){
+						break;
+					}
+					line = line.Replace("\\","/");
+						
+					_queue.Add(delegate() {
+						req.Log(0,line);
+					});
+
+				}while(true);
+
+				while(true){
+					string error = p.StandardError.ReadLine();
+					if(string.IsNullOrEmpty(error)){
+						break;
+					}
+					hasError = true;
+					_queue.Add(delegate() {
+						req.Log(1,error);
+					});
+				}
+				p.Close();
+				if(hasError){
+					_queue.Add(delegate() {
+						req.Error();
+					});
+				}
+				else {
+					_queue.Add(delegate() {
+						req.NotifyDone();
+					});
+				} 
+
+				
+			}catch(System.Exception e){
+				UnityEngine.Debug.LogException(e);
+				if(p != null){
+					p.Close();
+				}
+			}
+		});
+		return req;
+	}
+
+	
+	private List<string> _enviroumentVars = new List<string>();
+	
+	public void AddEnvironmentVars(params string[] vars){
+		for(int i = 0;i<vars.Length;i++){
+			if(string.IsNullOrEmpty(vars[i].Trim())){
+				continue;
+			}
+			_enviroumentVars.Add(vars[i]);
+		}
+	}
+
+	public ShellRequest ProcessCMD(string cmd,string workDir){
+		return ShellHelper.ProcessCommand(cmd,workDir,_enviroumentVars);
+	}
+
+}
---
+++ Editor/ShellHelperTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ShellHelperTest  {
+
+
+	[MenuItem("ShellHelperTest/ls")]
+	public static void ls(){
+#if UNITY_EDITOR_OSX
+		ShellHelper.ShellRequest req = ShellHelper.ProcessCommand("ls","");
+		req.onLog += delegate(int arg1, string arg2) {
+			Debug.Log(arg2);
+		}; 
+
+#endif
+	}
+	[MenuItem("ShellHelperTest/dir")]
+	public static void dir(){
+		#if UNITY_EDITOR_WIN
+		ShellHelper.ShellRequest req = ShellHelper.ProcessCommand("dir","");
+		req.onLog += delegate(int arg1, string arg2) {
+			Debug.Log(arg2);
+		};
+		
+		#endif
+	}
+
+}
